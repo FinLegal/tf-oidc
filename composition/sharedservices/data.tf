4db12d98d2ae23da6a640d0088802fc2bb11c8fb
Callum Everatt <callum.everatt@finlegal.io>
2024-12-23 13:05:22 +0000

    oidc lza sharedservices

---
+++ composition/sharedservices/data.tf
@@ -0,0 +1,242 @@
+########################################
+# Data Sources
+########################################
+
+data "aws_region" "this" {}
+data "aws_caller_identity" "this" {}
+data "scalr_current_run" "this" {}
+
+#data "terraform_remote_state" "storybook" {
+#  backend = "remote"
+
+#  config = {
+#    hostname     = "finlegal.scalr.io"
+#    organization = data.scalr_current_run.this.environment_id
+#    workspaces = {
+#      name = "Storybook"
+#    }
+#  }
+#}
+
+data "terraform_remote_state" "casesite_ecr" {
+  backend = "remote"
+
+  config = {
+    hostname     = "finlegal.scalr.io"
+    organization = data.scalr_current_run.this.environment_id
+    workspaces = {
+      name = "ECR-Case-sites"
+    }
+  }
+}
+
+########################################
+# Computed Variables
+########################################
+
+locals {
+  ## GitHub OIDC ##
+  aws_github_audience = {
+    "token" = {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+
+  ## Objects (These will be moved to remote state as we migrate to Scalr) ##
+  #storybook_bucket           = data.terraform_remote_state.storybook.outputs.s3_arn
+  #internal_docs_bucket       = "arn:aws:s3:::finlegal-internal"
+  #public_docs_bucket         = "arn:aws:s3:::finlegal-docs"
+  #casesite_deployment_bucket = "arn:aws:s3:::generic-cases-deployments"
+  #static_bucket              = "arn:aws:s3:::casefunnel-io-static"
+
+  ## Casesites ##
+  casesite_ecr              = split("/", data.terraform_remote_state.casesite_ecr.outputs.aws_ecr_repository)
+  casesite_ecr_arn          = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/${local.casesite_ecr[1]}"
+  casesite_release_branches = ["main", "release-generic", "release-hf-trades", "release-kl-merc", "release-kl-vauxhall", "release-lsc-energy", "release-mb-floods", "lza"]
+
+  ### Casefunnel ##
+  #casefunnel_admin_ecr_arn      = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/casefunnel-admin"
+  #casefunnel_api_ecr_arn        = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/casefunnel-api"
+  #casefunnel_dashboard_ecr_arn  = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/casefunnel-dashboard"
+  #casefunnel_systemjobs_ecr_arn = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/casefunnel-systemjobs"
+  #casefunnel_userjobs_ecr_arn   = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/casefunnel-userjobs"
+  #casefunnel_ecr_arn_list       = [local.casefunnel_admin_ecr_arn, local.casefunnel_api_ecr_arn, local.casefunnel_dashboard_ecr_arn, local.casefunnel_userjobs_ecr_arn, local.casefunnel_systemjobs_ecr_arn]
+
+  ## Legacy Casesites ##
+  ## ECR
+  #hf_trades_ecr_arn       = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-hf-trades"
+  #hf_trades_release_arn   = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-hf-trades-release"
+  #kl_merc_ecr_arn         = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-kl-merc"
+  #kl_merc_release_arn     = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-kl-merc-release"
+  #kl_vauxhall_ecr_arn     = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-kl-vauxhall"
+  #kl_vauxhall_release_arn = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-kl-vauxhall-release"
+  #lsc_energy_ecr_arn      = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-lsc-energy"
+  #lsc_energy_release_arn  = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-lsc-energy-release"
+  #mb_floods_ecr_arn       = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-mb-floods"
+  #mb_floods_release_arn   = "arn:aws:ecr:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:repository/case-site-mb-floods-release"
+  #legacy_ecr_arn_list     = [local.hf_trades_ecr_arn, local.hf_trades_release_arn, local.kl_merc_ecr_arn, local.kl_merc_release_arn, local.kl_vauxhall_ecr_arn, local.kl_vauxhall_release_arn, local.lsc_energy_ecr_arn, local.lsc_energy_release_arn, local.mb_floods_ecr_arn, local.mb_floods_release_arn]
+}
+
+########################################
+# Policies
+########################################
+
+#data "aws_iam_policy_document" "this_terraform" {
+#  statement {
+#    sid         = "DisallowAdminActions"
+#    effect      = "Allow"
+#    not_actions = ["organizations:*", "account:*"]
+#    resources   = ["*"]
+#  }
+#  statement {
+#    sid    = "AllowBasicActions"
+#    effect = "Allow"
+#    actions = [
+#      "account:GetAccountInformation",
+#      "account:GetPrimaryEmail",
+#      "account:ListRegions",
+#      "organizations:DescribeOrganization"
+#    ]
+#    resources = ["*"]
+#  }
+#}
+
+#data "aws_iam_policy_document" "this_storybook" {
+#  statement {
+#    sid       = "AllowList"
+#    effect    = "Allow"
+#    actions   = ["s3:ListBucket"]
+#    resources = [local.storybook_bucket]
+#  }
+#  statement {
+#    sid       = "AllowPut"
+#    effect    = "Allow"
+#    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+#    resources = ["${local.storybook_bucket}/*"]
+#  }
+#}
+
+#data "aws_iam_policy_document" "this_docs_private" {
+#  statement {
+#    sid       = "AllowList"
+#    effect    = "Allow"
+#    actions   = ["s3:ListBucket"]
+#    resources = [local.internal_docs_bucket]
+#  }
+#  statement {
+#    sid       = "AllowPut"
+#    effect    = "Allow"
+#    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+#    resources = ["${local.internal_docs_bucket}/*"]
+#  }
+#}
+
+#data "aws_iam_policy_document" "this_docs_public" {
+#  statement {
+#    sid       = "AllowList"
+#    effect    = "Allow"
+#    actions   = ["s3:ListBucket"]
+#    resources = [local.public_docs_bucket]
+#  }
+#  statement {
+#    sid       = "AllowPut"
+#    effect    = "Allow"
+#    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+#    resources = ["${local.public_docs_bucket}/*"]
+#  }
+#}
+
+#data "aws_iam_policy_document" "this_case_deployment" {
+#  statement {
+#    sid       = "AllowList"
+#    effect    = "Allow"
+#    actions   = ["s3:ListBucket"]
+#    resources = [local.casesite_deployment_bucket]
+#  }
+#  statement {
+#    sid       = "AllowPut"
+#    effect    = "Allow"
+#    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+#    resources = ["${local.casesite_deployment_bucket}/*"]
+#  }
+#}
+
+#data "aws_iam_policy_document" "this_static_deployment" {
+#  statement {
+#    sid       = "AllowList"
+#    effect    = "Allow"
+#    actions   = ["s3:ListBucket"]
+#    resources = [local.static_bucket]
+#  }
+#  statement {
+#    sid       = "AllowPut"
+#    effect    = "Allow"
+#    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:GetObjectAcl", "s3:PutObjectAcl"]
+#    resources = ["${local.static_bucket}/*"]
+#  }
+#}
+
+data "aws_iam_policy_document" "this_casesite_ecr" {
+  statement {
+    sid       = "AllowAuthToken"
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowECR"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage"
+    ]
+    resources = [local.casesite_ecr_arn]
+  }
+}
+
+#data "aws_iam_policy_document" "this_casesite_ecr_legacy" {
+#  statement {
+#    sid       = "AllowAuthToken"
+#    effect    = "Allow"
+#    actions   = ["ecr:GetAuthorizationToken"]
+#    resources = ["*"]
+#  }
+#  statement {
+#    sid    = "AllowECR"
+#   effect = "Allow"
+#   actions = [
+#     "ecr:BatchCheckLayerAvailability",
+#      "ecr:CompleteLayerUpload",
+#      "ecr:UploadLayerPart",
+#      "ecr:InitiateLayerUpload",
+#      "ecr:PutImage"
+#    ]
+#    resources = local.legacy_ecr_arn_list
+#  }
+#}
+
+#data "aws_iam_policy_document" "this_casefunnel_ecr" {
+#  statement {
+#    sid       = "AllowAuthToken"
+#    effect    = "Allow"
+#    actions   = ["ecr:GetAuthorizationToken"]
+#    resources = ["*"]
+#  }
+#  statement {
+#    sid    = "AllowECR"
+#    effect = "Allow"
+#    actions = [
+#      "ecr:BatchCheckLayerAvailability",
+#      "ecr:CompleteLayerUpload",
+#      "ecr:UploadLayerPart",
+#      "ecr:InitiateLayerUpload",
+#      "ecr:PutImage"
+#    ]
+#    resources = local.casefunnel_ecr_arn_list
+#  }
+#}
---
+++ composition/sharedservices/main.tf
@@ -0,0 +1,92 @@
+########################################
+# GitHub OIDC
+########################################
+
+module "git" {
+  source  = "finlegal.scalr.io/acc-v0od9n5ghtfveu0dj/oidc/aws"
+  version = "1.0.22-beta.14"
+
+  name_prefix = local.name_prefix
+  oidc_provider_configuration = {
+    url             = "https://token.actions.githubusercontent.com"
+    client_id_list  = ["sts.amazonaws.com"]
+    thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+  }
+  provider_role_configuration = {
+    # ## legacy github terraform (infra repo) ##
+    # "terraform" = {
+    #   policies = {
+    #     "terraform" = {
+    #       policy = data.aws_iam_policy_document.this_terraform.json
+    #     }
+    #   }
+    #   conditions = merge(local.aws_github_audience, { "repo" = { test = "StringLike", variable = "token.actions.githubusercontent.com:sub", values = ["repo:FinLegal/infrastructure:*"] } })
+    # }
+    ## Storybook ##
+    #"storybook" = {
+    #  policies = {
+    #    "s3" = {
+    #      policy = data.aws_iam_policy_document.this_storybook.json
+    #    }
+    #  }
+    #  conditions = merge(local.aws_github_audience, { "repo" = { test = "StringEquals", variable = "token.actions.githubusercontent.com:sub", values = ["repo:FinLegal/casefunnel:ref:refs/heads/dev"] } })
+    #}
+    ## Docs - Private ##
+    #"docs-private" = {
+    #  policies = {
+    #    "s3" = {
+    #      policy = data.aws_iam_policy_document.this_docs_private.json
+    #    }
+    #  }
+    #  conditions = merge(local.aws_github_audience, { "repo" = { test = "StringEquals", variable = "token.actions.githubusercontent.com:sub", values = ["repo:FinLegal/docs:ref:refs/heads/main"] } })
+    #}
+    ## Docs - Private ##
+    #"docs-public" = {
+    #  policies = {
+    #    "s3" = {
+    #      policy = data.aws_iam_policy_document.this_docs_public.json
+    #    }
+    #  }
+    #  conditions = merge(local.aws_github_audience, { "repo" = { test = "StringEquals", variable = "token.actions.githubusercontent.com:sub", values = ["repo:FinLegal/docs:ref:refs/heads/main"] } })
+    #}
+    ## Case Site Deployments ##
+    #"cases" = {
+    #  policies = {
+    #    "s3" = {
+    #      policy = data.aws_iam_policy_document.this_case_deployment.json
+    #    }
+    #  }
+    #  conditions = merge(local.aws_github_audience, { "repo" = { test = "StringEquals", variable = "token.actions.githubusercontent.com:sub", values = ["repo:FinLegal/case-definitions:ref:refs/heads/main"] } })
+    #}
+    ## Static Content ##
+    #"static" = {
+    #  policies = {
+    #    s3 = {
+    #      policy = data.aws_iam_policy_document.this_static_deployment.json
+    #    }
+    #  }
+    #  conditions = merge(local.aws_github_audience, { "repo" = { test = "StringEquals", variable = "token.actions.githubusercontent.com:sub", values = ["repo:FinLegal/casefunnel-static:ref:refs/heads/master"] } })
+    #}
+    ## Case Site ECR ##
+    "casesite-ecr" = {
+      policies = {
+        ecr = {
+          policy = data.aws_iam_policy_document.this_casesite_ecr.json
+        }
+        #legacy = {
+        #  policy = data.aws_iam_policy_document.this_casesite_ecr_legacy.json
+        #}
+      }
+      conditions = merge(local.aws_github_audience, { "repo" = { test = "StringEquals", variable = "token.actions.githubusercontent.com:sub", values = [for b in local.casesite_release_branches : "repo:FinLegal/case-sites:ref:refs/heads/${b}"] } })
+    }
+    #    ## CaseFunnel ECR ##
+    #    "casefunnel-ecr" = {
+    #      policies = {
+    #        ecr = {
+    #          policy = data.aws_iam_policy_document.this_casefunnel_ecr.json
+    #        }
+    #      }
+    #      conditions = merge(local.aws_github_audience, { "repo" = { test = "StringEquals", variable = "token.actions.githubusercontent.com:sub", values = ["repo:FinLegal/casefunnel:ref:refs/heads/dev", "repo:FinLegal/casefunnel:ref:refs/heads/master", "repo:FinLegal/casefunnel:ref:refs/heads/release"] } })
+    #    }
+  }
+}
