56f92640b9fc127a894bab5a4efae6112a02c473
Callum Everatt <callum.everatt@finlegal.io>
2025-06-02 14:05:50 +0100

    Fix

--- composition/workload/terraform.tf
+++ composition/workload/terraform.tf
@@ -9,13 +9,5 @@ terraform {
       version = "~> 1.13"
     }
   }
-  cloud {
-    hostname     = "finlegal.scalr.io"
-    organization = "env-v0ojirg3vo70j46ua"
-
-    workspaces {
-      name = "OIDC"
-    }
-  }
   required_version = "~> 1.8"
 }
